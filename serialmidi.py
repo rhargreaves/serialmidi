6793e1a154406b42731755ba29ca4bd600e7395c
Robert Hargreaves <kiyouta@gmail.com>
2026-02-02 13:34:46 +0000

    build: implement changes to support PyPi packaging (wip)

--- serialmidi.py
+++ serialmidi.py
@@ -5,7 +5,6 @@ import serial
 import threading
 import logging
 import sys
-import time
 import argparse
 
 # Serial MIDI Bridge
@@ -14,46 +13,17 @@ import argparse
 # Licensed under Apache License 2.0
 
 
-parser = argparse.ArgumentParser(description="Serial MIDI bridge")
+class midi_input_handler(object):
+    def __init__(self, port, midiin_message_queue):
+        self.port = port
+        self._wallclock = time.time()
+        self.midiin_message_queue = midiin_message_queue
 
-parser.add_argument(
-    "--serial_name", type=str, required=True, help="Serial port name. Required"
-)
-parser.add_argument(
-    "--baud", type=int, default=115200, help="baud rate. Default is 115200"
-)
-parser.add_argument("--midi_in_name", type=str, default="IAC Bus 1")
-parser.add_argument("--midi_out_name", type=str, default="IAC Bus 2")
-parser.add_argument(
-    "--debug", action="store_true", help="Print incoming / outgoing MIDI signals"
-)
-parser.add_argument(
-    "--string", action="store_true", help="Print sysEx logging message (For Qun Mk2)"
-)
-parser.add_argument(
-    "--everdrive_pro",
-    action="store_true",
-    help="Format serial data for delivery to the Mega Everdrive PRO",
-)
-
-args = parser.parse_args()
-
-thread_running = True
-
-# Arguments
-serial_port_name = args.serial_name  #'/dev/cu.SLAB_USBtoUART'
-serial_baud = args.baud
-given_port_name_in = args.midi_in_name  # "IAC Bus 1"
-given_port_name_out = args.midi_out_name  # "IAC Bus 2"
-
-if args.debug:
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.INFO)
-
-midi_ready = False
-midiin_message_queue = queue.Queue()
-midiout_message_queue = queue.Queue()
+    def __call__(self, event, data=None):
+        message, deltatime = event
+        self._wallclock += deltatime
+        # logging.debug("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
+        self.midiin_message_queue.put(message)
 
 
 def get_midi_length(message):
@@ -86,137 +56,178 @@ def wrap_message_for_mega_pro(message):
     return header + len_bytes + padding + message
 
 
-def serial_writer():
-    while midi_ready == False:
-        time.sleep(0.1)
-    while thread_running:
+class SerialMidi:
+    def __init__(self, args):
+        self.thread_running = True
+        self.serial_port_name = args.serial_name  #'/dev/cu.SLAB_USBtoUART'
+        self.serial_baud = args.baud
+        self.given_port_name_in = args.midi_in_name  # "IAC Bus 1"
+        self.given_port_name_out = args.midi_out_name  # "IAC Bus 2"
+        self.everdrive_pro = args.everdrive_pro
+        self.string = args.string
+        self.midi_ready = False
+        self.midiin_message_queue = queue.Queue()
+        self.midiout_message_queue = queue.Queue()
+
+    def serial_writer(self):
+        while not self.midi_ready:
+            time.sleep(0.1)
+        while self.thread_running:
+            try:
+                message = self.midiin_message_queue.get(timeout=0.4)
+            except queue.Empty:
+                continue
+            logging.debug("out: " + str(message))
+            value = bytearray(message)
+            if self.everdrive_pro:
+                value = wrap_message_for_mega_pro(value)
+            self.ser.write(value)
+
+    def serial_watcher(self):
+        receiving_message = []
+        running_status = 0
+
+        while not self.midi_ready:
+            time.sleep(0.1)
+
+        while self.thread_running:
+            data = self.ser.read()
+            if data:
+                for elem in data:
+                    receiving_message.append(elem)
+                # Running status
+                if len(receiving_message) == 1:
+                    if (receiving_message[0] & 0xF0) != 0:
+                        running_status = receiving_message[0]
+                    else:
+                        receiving_message = [running_status, receiving_message[0]]
+
+                message_length = get_midi_length(receiving_message)
+                if message_length <= len(receiving_message):
+                    logging.debug("in: " + str(receiving_message))
+                    self.midiout_message_queue.put(receiving_message)
+
+                    if self.string:
+                        if receiving_message[0] == 0xF0:
+                            print_message = []
+                            for elem in receiving_message:
+                                if elem < 0xF0:
+                                    print_message.append(chr(elem))
+                            print_message_str = "".join(print_message)
+                            print(print_message_str)
+                    receiving_message = []
+
+    def midi_watcher(self):
+        midiin = rtmidi.MidiIn()
+        midiout = rtmidi.MidiOut()
+        available_ports_out = midiout.get_ports()
+        available_ports_in = midiin.get_ports()
+        logging.info("IN : '" + "','".join(available_ports_in) + "'")
+        logging.info("OUT : '" + "','".join(available_ports_out) + "'")
+        if self.everdrive_pro:
+            logging.info("Mega Everdrive PRO mode enabled")
+        logging.info("Hit ctrl-c to exit")
+
+        port_index_in = -1
+        port_index_out = -1
+        for i, s in enumerate(available_ports_in):
+            if self.given_port_name_in in s:
+                port_index_in = i
+        for i, s in enumerate(available_ports_out):
+            if self.given_port_name_out in s:
+                port_index_out = i
+
+        if port_index_in == -1:
+            print("MIDI IN Device name is incorrect. Please use listed device name.")
+        if port_index_out == -1:
+            print("MIDI OUT Device name is incorrect. Please use listed device name.")
+        if port_index_in == -1 or port_index_out == -1:
+            self.thread_running = False
+            self.midi_ready = True
+            sys.exit()
+
+        midiout.open_port(port_index_out)
+        in_port_name = midiin.open_port(port_index_in)
+
+        self.midi_ready = True
+
+        midiin.ignore_types(sysex=False, timing=False, active_sense=False)
+        midiin.set_callback(midi_input_handler(in_port_name, self.midiin_message_queue))
+
+        while self.thread_running:
+            try:
+                message = self.midiout_message_queue.get(timeout=0.4)
+            except queue.Empty:
+                continue
+            midiout.send_message(message)
+
+    def start(self):
         try:
-            message = midiin_message_queue.get(timeout=0.4)
-        except queue.Empty:
-            continue
-        logging.debug("out: " + str(message))
-        value = bytearray(message)
-        if args.everdrive_pro:
-            value = wrap_message_for_mega_pro(value)
-        ser.write(value)
+            self.ser = serial.Serial(self.serial_port_name, self.serial_baud)
+        except serial.serialutil.SerialException:
+            print("Serial port opening error.")
+            self.midi_watcher()
+            sys.exit()
+
+        self.ser.timeout = 0.4
+
+        self.s_watcher = threading.Thread(target=self.serial_watcher)
+        self.s_writer = threading.Thread(target=self.serial_writer)
+        self.m_watcher = threading.Thread(target=self.midi_watcher)
+
+        self.s_watcher.start()
+        self.s_writer.start()
+        self.m_watcher.start()
+
+    def stop(self):
+        self.thread_running = False
 
 
-def serial_watcher():
-    receiving_message = []
-    running_status = 0
-
-    while midi_ready == False:
-        time.sleep(0.1)
-
-    while thread_running:
-        data = ser.read()
-        if data:
-            for elem in data:
-                receiving_message.append(elem)
-            # Running status
-            if len(receiving_message) == 1:
-                if (receiving_message[0] & 0xF0) != 0:
-                    running_status = receiving_message[0]
-                else:
-                    receiving_message = [running_status, receiving_message[0]]
-
-            message_length = get_midi_length(receiving_message)
-            if message_length <= len(receiving_message):
-                logging.debug("in: " + str(receiving_message))
-                midiout_message_queue.put(receiving_message)
-
-                if args.string:
-                    if receiving_message[0] == 0xF0:
-                        print_message = []
-                        for elem in receiving_message:
-                            if elem < 0xF0:
-                                print_message.append(chr(elem))
-                        print_message_str = "".join(print_message)
-                        print(print_message_str)
-                receiving_message = []
+def parse_args():
+    parser = argparse.ArgumentParser(description="Serial MIDI bridge")
+    parser.add_argument(
+        "--serial_name", type=str, required=True, help="Serial port name. Required"
+    )
+    parser.add_argument(
+        "--baud", type=int, default=115200, help="baud rate. Default is 115200"
+    )
+    parser.add_argument("--midi_in_name", type=str, default="IAC Bus 1")
+    parser.add_argument("--midi_out_name", type=str, default="IAC Bus 2")
+    parser.add_argument(
+        "--debug", action="store_true", help="Print incoming / outgoing MIDI signals"
+    )
+    parser.add_argument(
+        "--string",
+        action="store_true",
+        help="Print sysEx logging message (For Qun Mk2)",
+    )
+    parser.add_argument(
+        "--everdrive_pro",
+        action="store_true",
+        help="Format serial data for delivery to the Mega Everdrive PRO",
+    )
+    return parser.parse_args()
 
 
-class midi_input_handler(object):
-    def __init__(self, port):
-        self.port = port
-        self._wallclock = time.time()
+def main():
+    args = parse_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    def __call__(self, event, data=None):
-        message, deltatime = event
-        self._wallclock += deltatime
-        # logging.debug("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
-        midiin_message_queue.put(message)
+    serialmidi = SerialMidi(args)
+    serialmidi.start()
 
-
-def midi_watcher():
-    global midi_ready, thread_running
-
-    midiin = rtmidi.MidiIn()
-    midiout = rtmidi.MidiOut()
-    available_ports_out = midiout.get_ports()
-    available_ports_in = midiin.get_ports()
-    logging.info("IN : '" + "','".join(available_ports_in) + "'")
-    logging.info("OUT : '" + "','".join(available_ports_out) + "'")
-    if args.everdrive_pro:
-        logging.info("Mega Everdrive PRO mode enabled")
-    logging.info("Hit ctrl-c to exit")
-
-    port_index_in = -1
-    port_index_out = -1
-    for i, s in enumerate(available_ports_in):
-        if given_port_name_in in s:
-            port_index_in = i
-    for i, s in enumerate(available_ports_out):
-        if given_port_name_out in s:
-            port_index_out = i
-
-    if port_index_in == -1:
-        print("MIDI IN Device name is incorrect. Please use listed device name.")
-    if port_index_out == -1:
-        print("MIDI OUT Device name is incorrect. Please use listed device name.")
-    if port_index_in == -1 or port_index_out == -1:
-        thread_running = False
-        midi_ready = True
-        sys.exit()
-
-    midiout.open_port(port_index_out)
-    in_port_name = midiin.open_port(port_index_in)
-
-    midi_ready = True
-
-    midiin.ignore_types(sysex=False, timing=False, active_sense=False)
-    midiin.set_callback(midi_input_handler(in_port_name))
-
-    while thread_running:
-        try:
-            message = midiout_message_queue.get(timeout=0.4)
-        except queue.Empty:
-            continue
-        midiout.send_message(message)
+    # Ctrl-C handler
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Terminating.")
+        serialmidi.stop()
+        sys.exit(0)
 
 
-try:
-    ser = serial.Serial(serial_port_name, serial_baud)
-except serial.serialutil.SerialException:
-    print("Serial port opening error.")
-    midi_watcher()
-    sys.exit()
-
-ser.timeout = 0.4
-
-s_watcher = threading.Thread(target=serial_watcher)
-s_writer = threading.Thread(target=serial_writer)
-m_watcher = threading.Thread(target=midi_watcher)
-
-s_watcher.start()
-s_writer.start()
-m_watcher.start()
-
-# Ctrl-C handler
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Terminating.")
-    thread_running = False
-    sys.exit(0)
+if __name__ == "__main__":
+    main()
